e50ec4af6a05196235e01bc5482fe90030d3dc6a
Yosef <110300438+fffdssaadasda@users.noreply.github.com>
2025-01-14 18:47:15 +0300

    Update ErrorMessage.tsx

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,6 +1,9 @@
 const ErrorMessage = ({ msg }: { msg: string }) => {
     return (
-        <div className="text-red-500 font-bold">{msg}</div>
+        <div className="text-red-500 font-bold max-sm:text-[22px] text-[3vw]">
+            سيتم  حل المشكله قريبا .... 
+{/*  msg} */}
+        </div>
     )
 }
 
