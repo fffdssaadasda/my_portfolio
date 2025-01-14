e70f8db701177d870e73d1ed8593cf78e088428f
Yosef <110300438+fffdssaadasda@users.noreply.github.com>
2025-01-14 18:05:42 +0300

    Update vas.ts

--- src/utils/vas.ts
+++ src/utils/vas.ts
@@ -1,2 +1,2 @@
 export const mainUrl = `http://localhost:5000`;
-export const api = `https://api-v2-production-0b14.up.railway.app`;
+export const api = `https://portfolioapi-production-84ea.up.railway.app`;
