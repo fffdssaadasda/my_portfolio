46b298b1a6e97845a25a8545417369cfac5ec844
Yosef <110300438+fffdssaadasda@users.noreply.github.com>
2025-01-14 18:54:15 +0300

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,7 @@ const error = ({ error, }: { error: Error & { digest?: string } }) => {
   return (
     // <ErrorPage message={error.message} statusCode={401} />
     <>
-     <div className="text-red-500 font-bold max-sm:text-[22px] text-[3vw]">
+     <div className="text-red-500 text-center font-bold max-sm:text-[22px] text-[3vw]">
             سيتم  حل المشكله قريبا .... 
         </div>
     </>
